256893f52a059c9bce34fec137c45ceba3bae412
Gyanle Yao <59084654+Leouas@users.noreply.github.com>
2020-02-24 16:02:38 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,14 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "Gyanle Yao"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+author = "Leouas"
+email = "Leoua7@163.com"
+author_homepage = "https://Leouas.top"
+description = "世上有千万种等待，最好的那种叫来日可期。"
+key_words = ['Leouas','Leoua7', 'blog']
 language = 'zh-CN'
 external_links = [
     {
